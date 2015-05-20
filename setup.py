7357cb8f7e87881146c3ff5a3158f1c4a8f1456d
jacob <jacob@fetchtv.com.au>
2015-05-20 16:37:57 +1000

    1.0 release

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #from distutils.core import setup
 from setuptools import setup
 setup(name='magentouch',
-      version='1.0.0',
+      version='1.0',
       description='Magento Soup v2 suds client',
       author='Jacob CUI',
       author_email='jacobcui123@gmail.com',
-      url='https://www.python.org/sigs/distutils-sig/',
+      url='https://github.com/jacobcui/magentouch',
       packages=['magentouch'],
       install_requires=['suds'],
       license='GPL',
